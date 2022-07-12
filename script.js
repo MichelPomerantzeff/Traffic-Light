2077711b802d183284dd4a29daea2c788ec7c7fb
Michel Pomerantzeff <micheldauniao@hotmail.com>
2022-07-12 21:17:30 +0100

    added light features on button click

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
 const trafficLight = document.getElementById('trafficLight')
 
+let redLight = document.createElement('div')
+redLight.classList.add("red")
 
-for (let i = 0; i < 3; i++) {
-    let lightDiv = document.createElement('div')
+let yellowLight = document.createElement('div')
+yellowLight.classList.add("yellow")
 
-    if (i === 0) {
-        lightDiv.classList.add("light")
-        lightDiv.classList.add("red-off")
-        trafficLight.appendChild(lightDiv)
-    }
+let greenLight = document.createElement('div')
+greenLight.classList.add("green")
 
-    if (i === 1) {
-        lightDiv.classList.add("light")
-        lightDiv.classList.add("yellow-off")
-        trafficLight.appendChild(lightDiv)
-    }
-    
-    if (i === 2) {
-        lightDiv.classList.add("light")
-        lightDiv.classList.add("green-off")
-        trafficLight.appendChild(lightDiv)
-    }
+trafficLight.appendChild(redLight)
+trafficLight.appendChild(yellowLight)
+trafficLight.appendChild(greenLight)
 
+const red = document.getElementById('red')
+const yellow = document.getElementById('yellow')
+const green = document.getElementById('green')
+const auto = document.getElementById('auto')
+
+const redOn = () => {
+    redLight.classList.add('red-on')
+    yellowLight.classList.remove('yellow-on')
+    greenLight.classList.remove('green-on')
 }
+
+const yellowOn = () => {
+    yellowLight.classList.add('yellow-on')
+    greenLight.classList.remove('green-on')
+    redLight.classList.remove('red-on')
+}
+
+const greenOn = () => {
+    greenLight.classList.add('green-on')
+    redLight.classList.remove('red-on')
+    yellowLight.classList.remove('yellow-on')
+}
+
+let intervalId = null
+let index = 0
+
+function alternateColors () {
+    if(index === 0) {
+        redOn()
+    } else if(index === 1) {
+        yellowOn()
+    } else {
+        greenOn()
+    }
+    index < 2 ? index ++ : index = 0
+}
+
+
+red.onclick = () => {
+    redOn()
+    clearInterval(intervalId)
+}
+
+yellow.onclick = () => {
+    yellowOn()
+    clearInterval(intervalId)
+}
+green.onclick = () => {
+    greenOn()
+    clearInterval(intervalId)
+}
+
+auto.onclick = () => intervalId = setInterval(alternateColors, 1000)
+
+
+
+
+
 
 
 
@@ -55,7 +103,3 @@ for (let i = 0; i < 3; i++) {
 //     'green': () => img.src = './img/green.png',
 //     'auto': () => intervalId = setInterval(alternateColors, 1000)
 // }
-
-
-
-// buttons.addEventListener('click', trafficLight)
